23184e492d0e61b9bb7dac312a22a88073dd64bd
nshutijonathan <nshutijonathan130@gmail.com>
2022-10-04 14:37:16 +0200

    enhanced production

--- src/helpers/users.js
+++ src/helpers/users.js
@@ -1,8 +1,8 @@
 import axios from "axios";
+const token = process.env.REACT_APP_GITHUB_TOKEN;
 const APIURL = process.env.REACT_APP_APIURL;
 const APIURLSEARCH = process.env.REACT_APP_APIURLSEARCH;
 const APIURLSEARCHREPOSITORIES = process.env.REACT_APP_APIURLSEARCHREPOSITORIES;
-let token = process.env.REACT_APP_GITHUB_TOKEN;
 const config = {
   headers: {
     Authorization: `Bearer ${token}`,
@@ -17,11 +17,11 @@ export const getUser = async (username) => {
   return data;
 };
 export const searchUsers = async (query) => {
-  const { data } = await axios.get(`${APIURLSEARCH}?q=${query}`);
+  const { data } = await axios.get(`${APIURLSEARCH}?q=${query}`, config);
   return data;
 };
 export const searchRepos = async () => {
-  const { data } = await axios.get(`${APIURLSEARCHREPOSITORIES}`);
+  const { data } = await axios.get(`${APIURLSEARCHREPOSITORIES}`, config);
 
   return data;
 };
